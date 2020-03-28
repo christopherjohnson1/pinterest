e9bd74b482fceb4460883d67477beb2938c22cee
Chris Johnson <christopherjohnson1@gmail.com>
2020-03-27 21:08:32 -0500

    added on auth state changed function

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,9 +3,11 @@ import apiKeys from './helpers/apiKeys.json';
 import 'bootstrap';
 import '../styles/main.scss';
 import auth from './components/auth/auth';
+import authData from './helpers/data/authData';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
+  authData.checkLoginStatus();
   auth.loginButton();
 };
 
