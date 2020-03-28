4d9153293b1ec60e637882397f00a7fbc6fa0858
Chris Johnson <christopherjohnson1@gmail.com>
2020-03-27 21:20:56 -0500

    added logout button event

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,11 +4,13 @@ import 'bootstrap';
 import '../styles/main.scss';
 import auth from './components/auth/auth';
 import authData from './helpers/data/authData';
+import myNavbar from './components/myNavbar/myNavbar';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   authData.checkLoginStatus();
   auth.loginButton();
+  myNavbar.logoutEvent();
 };
 
 init();
